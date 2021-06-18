2863f099f5c6ac5b4e643d5f47cbd4cc764a060d
sures <suresh19493@gmail.com>
2021-06-18 14:14:57 +0530

    Initial commit

--- src/main/java/com/honeywell/test/mainApp/HoneyWellTestApplication.java
+++ src/main/java/com/honeywell/test/mainApp/HoneyWellTestApplication.java
@@ -2,8 +2,10 @@ package com.honeywell.test.mainApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.honeywell.test")
 public class HoneyWellTestApplication {
 
 	public static void main(String[] args) {
